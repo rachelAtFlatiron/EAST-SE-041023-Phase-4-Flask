d3b7d5b889ec3849b0a8961466bad07210b702c5
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-22 15:35:12 -0400

    aligned with final product

--- 01_intro_flask/server/models.py
+++ 01_intro_flask/server/models.py
@@ -5,5 +5,5 @@ db = SQLAlchemy()
 # 1. ✅ Create a Production Model
 	# tablename = 'Productions'
 	# Columns:
-        # title: string, genre: string, budget:float, image:string,director: string, description:string, ongoing:boolean, created_at:date time, updated_at: date time 
+        # title: string, genre: string, length:integer, year:integer, image:string, language:string, director: string, description:string, composer:string, created_at:date time, updated_at: date time 
 # 2. ✅ navigate to app.py
