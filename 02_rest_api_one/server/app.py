a8508eafee211accf76bbff597febfd48a1bad2b
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-22 17:42:14 -0400

    02_solution

--- 02_rest_api_one/server/app.py
+++ 02_rest_api_one/server/app.py
@@ -29,17 +29,80 @@ def index():
     # 1a. Create the query
     # 1b. Loop through the query and convert each object into a dictionary 
     # 1c. Use make_response and jsonify to return a response
-    # 3d. Test in Postman
+    # 1d. Test in Postman
+@app.route('/productions', methods=["GET", "POST"])
+# 🛑 emphasize this has to be a different class name than the model class
+def Productions():
+    if request.method == "GET":
+        q = Production.query.all()
+
+        # prods_list = []
+        # for prod in q:
+        #     prods_list.append({
+        #         "title": prod.title,
+        #         "genre": prod.genre,
+        #         "length": prod.length
+        #     })
+
+        # 5c. use SerializerMixin's .to_dict() for responses here and everywhere
+        prods_list = [p.to_dict() for p in q]
+
+        return make_response(jsonify(prods_list), 200)
 
     # 3. Create a route to /productions for a POST request
-    # 3a. Get information from request.form 
-    # 3b. Add and commit to db
-    # 3c. Return with make_response
-    # 3d. Test in postman
+    if request.method == "POST":
+        # 3a. Get information from request.get_json() 
+        # 🛑 review: request.args (key value pairs in query string)
+        # 🛑 review: request.form (key value pairs in body from HTML post form)
+        # 🛑 review: request.values (combines args, form)
+        # 🛑 review: request.json (parsed json data)
+            # 🛑 review: use request.get_json(force=True) to ignore content type
+        data = request.get_json() # or request.json
+        # 3b. Create new object
+        prod = Production(
+            # 🛑 review: request.__['key'] is for if you know key exists
+            # 🛑 review: otherwise use request.__.get('key')
+            title=data.get('title'),
+            year=data.get('year'),
+        )
+        # 3c. Add and commit to db 
+        db.session.add(prod)
+        db.session.commit()
+
+        # 3d. Convert to dictionary / # 5c. use .to_dict
+        prod_dict = {
+            "title": prod.title,
+            "year": prod.year,
+            "id": prod.id
+        }
+        # 3e. return as JSON
+        return make_response(jsonify(prod_dict), 201)
+        # 3f. Test in postman
 
 # 2. Create a route to /productions/:id for single GET requests
-    
+@app.route('/productions/<int:id>', methods=["GET", "DELETE"])
+def One_Production(id):
+    if request.method == "GET":
+        q = Production.query.filter_by(id=id).first()
+        # 5c. use to_dict
+        prod = {
+            "title": q.title,
+            "genre": q.genre,
+            "director": q.director,
+            "year": q.year
+        }
+
+        return make_response(jsonify(q.to_dict()), 200)
     # 4. Create a delete request 
+    if request.method == "DELETE":
+        q = Production.query.filter_by(id=id).first()
+        db.session.delete(q)
+        db.session.commit()
+        return make_response({}, 204)
+    
+# 5. Serializers: navigate back to models.py 
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- 02_rest_api_one/server/seed.py
+++ 02_rest_api_one/server/seed.py
@@ -1,31 +1,93 @@
 #!/usr/bin/env python3
 
+# 7c. uncomment everything regarding Role
+# 7d. Migrate and seed database
+# 7e. Check /productions route
 from app import app
-from models import Production, db
+from models import Production, Role, db
 
 with app.app_context():
+    Production.query.delete()
+    Role.query.delete()
+    menu = Production(title='the menu',
+                      genre='thriller',
+                      length=107,
+                      image='https://m.media-amazon.com/images/M/MV5BMzdjNjI5MmYtODhiNS00NTcyLWEzZmUtYzVmODM5YzExNDE3XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_FMjpg_UX1000_.jpg',
+                      language='english',
+                      director='mark mylod',
+                      description='Painstakingly prepared.  Brilliantly executed.',
+                      composer='colin stetson',
+                      year=2022
+                      )
 
-   Production.query.delete()
+    everything = Production(
+        title="everything everywhere all at once",
+        genre='sci-fi',
+        length=139,
+        image="https://m.media-amazon.com/images/M/MV5BYTdiOTIyZTQtNmQ1OS00NjZlLWIyMTgtYzk5Y2M3ZDVmMDk1XkEyXkFqcGdeQXVyMTAzMDg4NzU0._V1_FMjpg_UX1000_.jpg",
+        language='english, mandarin',
+        director='daniel kwan',
+        description='Sucked into a bagel.',
+        composer='son lux',
+        year=2022
+    )
+    guardians = Production(
+        title="guardians of the galaxy vol. 2",
+        genre='sci-fi',
+        length=137,
+        language='english',
+        image='https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_FMjpg_UX1000_.jpg',
+        composer='tyler bates',
+        year=2017,
+        director='james gunn',
+        description='when things get bad, they\'ll do their worst'
+    )
+    budapest = Production(
+        title='the grand budapest hotel',
+        genre='comedy',
+        length=100,
+        year=2014,
+        image='https://m.media-amazon.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_FMjpg_UX1000_.jpg',
+        language='english',
+        director='wes anderson',
+        composer='alexandre desplat',
+        description='keep your hands off my lobby boy!'
+    )
 
-   amsterdam = Production(
-       title="amsterdam",
-       genre="mystery",
-       image="https://m.media-amazon.com/images/M/MV5BNDQwNzE0ZTItYmZjMC00NjI3LWFlNzctNTExZDY2NWE0Zjc0XkEyXkFqcGdeQXVyMTA3MDk2NDg2._V1_FMjpg_UX1000_.jpg",
-       length=134,
-       director="david o russell",
-       composer="daniel pemberton",
-       description="let the love, murder, and conspiracy begin"
-   )
+    productions = [budapest, guardians, everything, menu]
+    db.session.add_all(productions)
+    db.session.commit()
 
-   nope = Production(
-       title='nope',
-       genre='sci-fi',
-       image='https://m.media-amazon.com/images/M/MV5BOGJhYzAwN2MtNjA1Ny00ZjJiLWFmNzYtMDgzNTUzYjc5NTIzXkEyXkFqcGdeQXVyMTUzOTcyODA5._V1_.jpg',
-       length=130,
-       director='jordan peele',
-       composer='michael abels',
-       description='nope'
-   )
 
-   db.session.add_all([amsterdam, nope])
-   db.session.commit()
+    margot = Role(
+        role_name='Margot',
+        production=menu
+    )
+    chef = Role(
+        role_name='Chef Slowik',
+        production=menu
+    )
+    evelyn = Role(
+        role_name='Evelyn Quan Wang',
+        production=everything
+    )
+    monsieur = Role(
+        role_name='Monsieur Gustave H.',
+        production=budapest
+    )
+    deputy = Role(
+        role_name="Deputy Vilmos Kovacs",
+        production=budapest
+    )
+    aleta = Role(
+        role_name='Aleta Ogord',
+        production=guardians
+    )
+    gamora = Role(
+        role_name='gamora',
+        production=guardians
+    )
+
+    roles = [gamora, aleta, deputy, monsieur, evelyn, chef, margot]
+    db.session.add_all(roles)
+    db.session.commit()
