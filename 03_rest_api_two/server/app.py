94cb9c54369441a3a4fef38d1518ae2b58299608
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-22 22:16:25 -0400

    removed answers

--- 03_rest_api_two/server/app.py
+++ 03_rest_api_two/server/app.py
@@ -6,7 +6,7 @@ from models import db, Production, Role, Actor
 from flask_restful import Api, Resource
 # 7a. import NotFound from werkzeug.exceptions and abort from Flask
 # 10a. import UnprocessableEntity
-from werkzeug.exceptions import NotFound, UnprocessableEntity
+
 
 
 app = Flask(__name__)
@@ -32,13 +32,13 @@ db.init_app(app)
 def index():
     return '<h1>Hello World!</h1>'
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 @app.route('/productions', methods=["GET", "POST"])
 def Productions():
     if(request.method=="GET"):
         # 7b. if not found, raise NotFound exception
         q = Production.query.all()
-        if not q:
-            raise NotFound
         prod_list = [p.to_dict() for p in q]
         res = make_response(jsonify(prod_list), 200)
         return res 
@@ -46,16 +46,11 @@ def Productions():
     if(request.method=="POST"):
         data = request.get_json()
         # 10b. if there's an exception, raise UnprocessableEntity
-        try:
-            prod = Production(title=data.get('title'), genre=data.get('genre'), length=data.get('length'), year=data.get('year'), image=data.get('image'), language=data.get('language'), director=data.get('director'), description=data.get('description'), composer=data.get('composer') )
-            db.session.add(prod)
-            db.session.commit()
-        except Exception:
-            raise UnprocessableEntity('no')
-
+        prod = Production(title=data.get('title'), genre=data.get('genre'), length=data.get('length'), year=data.get('year'), image=data.get('image'), language=data.get('language'), director=data.get('director'), description=data.get('description'), composer=data.get('composer') )
+        db.session.add(prod)
+        db.session.commit()
         dict = prod.to_dict()
         return make_response(jsonify(dict), 201)
-
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -64,8 +59,6 @@ def One_Production(id):
     if(request.method == 'GET'):
         q = Production.query.filter_by(id=id).first()
         # 7b. raise NotFound exception
-        if not q:
-            raise NotFound
         prod_dict = q.to_dict()
         res = make_response(jsonify(prod_dict), 200)
         return res
@@ -77,86 +70,79 @@ def One_Production(id):
         return make_response({}, 204)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 class Roles(Resource):
     
     def get(self):
         q = Role.query.all()
         # 7c. If not found, use abort
-        if not q:
-            abort(404, "The role was not found")
         role_dict = [r.to_dict(only=('id', 'role_name', 'actor.name', 'production.title')) for r in q]
         return make_response(role_dict, 200)
     
     def post(self):
         data = request.get_json()
         # 10b. add unprocessable entity
-        try:
-            role = Role(role_name=data.get('role_name'), production_id=data.get('production_id'))
-            db.session.add(role)
-            db.session.commit()
-        except Exception:
-            raise UnprocessableEntity('no')
-
+        role = Role(role_name=data.get('role_name'), production_id=data.get('production_id'))
+        db.session.add(role)
+        db.session.commit()
         return make_response(role.to_dict(), 201)
     
 api.add_resource(Roles, '/roles', '/test')
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class One_Role(Resource):
 
     def get(self, id):
         q = Role.query.filter_by(id=id).first()
         # 7c. If not found, use abort
-        if not q:
-            abort(404, "The role was not found")
         return make_response(q.to_dict(), 200)
+    
     def delete(self, id):
         q = Role.query.filter_by(id=id).first()
         db.session.delete(q)
         db.session.commit()
         return make_response({}, 204)
+    
     def patch(self, id):
         q = Role.query.filter_by(id=id).first()
         data = request.get_json()
         # 10b. raise unprocessable entity
-        try:
-            for attr in data:
-                setattr(q, attr, data.get(attr))
-            db.session.add(q)
-            db.session.commit()
-        except Exception:
-            raise UnprocessableEntity('no')
+        for attr in data:
+            setattr(q, attr, data.get(attr))
+        db.session.add(q)
+        db.session.commit()
+
         return make_response(q.to_dict(), 200)
     
 api.add_resource(One_Role, '/roles/<int:id>')
 
-# ~~~~~~~~~~~~~~~YOU DO~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 class Actors(Resource):
     def get(self):
         q = Actor.query.all()
-        if not q:
-            abort(404, "The Actor was not found")
+        # 7c. If not found, use abort
         actor_dict = [a.to_dict() for a in q]
         return make_response(actor_dict, 200)
     
     def post(self):
         data = request.get_json()
         # 10b. raise unprocessable entity
-        try:
-            actor = Actor(name=data.get('name'), image=data.get('image'), age=data.get('age'), country=data.get('country'))
-            db.session.add(actor)
-            db.session.commit()
-        except Exception:
-            raise UnprocessableEntity('no')
-
+        actor = Actor(name=data.get('name'), image=data.get('image'), age=data.get('age'), country=data.get('country'))
+        db.session.add(actor)
+        db.session.commit()
         return make_response(actor.to_dict(), 201)
+    
 api.add_resource(Actors, '/actors')
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class One_Actor(Resource):
 
     def get(self, id):
         q = Actor.query.filter_by(id=id).first()
-        if not q:
-            abort(404, "The Actor was not found")
+        # 7c. If not found, use abort
         return make_response(q.to_dict(), 200)
     def delete(self, id):
         q = Actor.query.filter_by(id=id).first()
@@ -168,26 +154,17 @@ class One_Actor(Resource):
         q = Actor.query.filter_by(id=id).first()
         data = request.get_json()
         # 10b. raise Unprocessable Entity
-        try:
-            for attr in data:
-                setattr(q, attr, data.get(attr))
-            db.session.add(q)
-            db.session.commit()
-        except Exception: 
-            raise UnprocessableEntity('no')
+        for attr in data:
+            setattr(q, attr, data.get(attr))
+        db.session.add(q)
+        db.session.commit()
         return make_response(q.to_dict(), 200)
 api.add_resource(One_Actor, '/actors/<int:id>')
-#~~~~~~~~~~~~~~~~~~~~~~~~~~END YOU DO~~~~~~~~~~~~~~~~~~~~~~~
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # 🛑 you still ned raise() and abort() so view method can exit gracefully
-#7d. create fallback
-@app.errorhandler(NotFound)
-def handle_not_found(e):
-    response = make_response(
-        "Not Found: Sorry the resource you are looking for does not exist",
-        404
-    )
-    return response
+#7d. create fallback with app.handler
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
