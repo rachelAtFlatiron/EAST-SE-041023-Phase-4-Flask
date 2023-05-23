71eb908cf293d66cf4d19a972ed23e17079af524
Rachel Katz <rachel.katz@flatironschool.com>
2023-05-23 11:01:24 -0400

    removed everything regarding 03 solution

--- 04_client_server/server/app.py
+++ 04_client_server/server/app.py
@@ -4,8 +4,6 @@ from flask import Flask, jsonify, make_response, request, abort
 from flask_migrate import Migrate 
 from models import db, Production, Role, Actor
 from flask_restful import Api, Resource
-# 7a. import NotFound from werkzeug.exceptions and abort from Flask
-# 10a. import UnprocessableEntity
 from werkzeug.exceptions import NotFound, UnprocessableEntity
 
 
@@ -13,20 +11,11 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
 
-# 🛑 render_as_batch=True prevents 'Constraint must have a name' on flask db migrate
 migrate = Migrate(app, db)
 
 api = Api(app)
 
 db.init_app(app)
-
-# | HTTP Verb 	|       Path       	| Description        	|
-# |-----------	|:----------------:	|--------------------	|
-# | GET       	|   /productions   	| READ all resources 	|
-# | GET       	| /productions/:id 	| READ one resource   	|
-# | POST      	|   /productions   	| CREATE one resource 	|
-# | PATCH/PUT 	| /productions/:id 	| UPDATE one resource	|
-# | DELETE    	| /productions/:id 	| DESTROY one resource 	|
 
 @app.route('/')
 def index():
@@ -35,7 +24,6 @@ def index():
 @app.route('/productions', methods=["GET", "POST"])
 def Productions():
     if(request.method=="GET"):
-        # 7b. if not found, raise NotFound exception
         q = Production.query.all()
         if not q:
             raise NotFound
@@ -45,7 +33,6 @@ def Productions():
     
     if(request.method=="POST"):
         data = request.get_json()
-        # 10b. if there's an exception, raise UnprocessableEntity
         try:
             prod = Production(title=data.get('title'), genre=data.get('genre'), length=data.get('length'), year=data.get('year'), image=data.get('image'), language=data.get('language'), director=data.get('director'), description=data.get('description'), composer=data.get('composer') )
             db.session.add(prod)
@@ -56,14 +43,12 @@ def Productions():
         dict = prod.to_dict()
         return make_response(jsonify(dict), 201)
 
-
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 @app.route('/productions/<int:id>', methods=["GET", "DELETE"])
 def One_Production(id):
     if(request.method == 'GET'):
         q = Production.query.filter_by(id=id).first()
-        # 7b. raise NotFound exception
         if not q:
             raise NotFound
         prod_dict = q.to_dict()
@@ -77,11 +62,11 @@ def One_Production(id):
         return make_response({}, 204)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 class Roles(Resource):
     
     def get(self):
         q = Role.query.all()
-        # 7c. If not found, use abort
         if not q:
             abort(404, "The role was not found")
         role_dict = [r.to_dict(only=('id', 'role_name', 'actor.name', 'production.title')) for r in q]
@@ -89,7 +74,6 @@ class Roles(Resource):
     
     def post(self):
         data = request.get_json()
-        # 10b. add unprocessable entity
         try:
             role = Role(role_name=data.get('role_name'), production_id=data.get('production_id'))
             db.session.add(role)
@@ -105,7 +89,6 @@ class One_Role(Resource):
 
     def get(self, id):
         q = Role.query.filter_by(id=id).first()
-        # 7c. If not found, use abort
         if not q:
             abort(404, "The role was not found")
         return make_response(q.to_dict(), 200)
@@ -117,7 +100,6 @@ class One_Role(Resource):
     def patch(self, id):
         q = Role.query.filter_by(id=id).first()
         data = request.get_json()
-        # 10b. raise unprocessable entity
         try:
             for attr in data:
                 setattr(q, attr, data.get(attr))
@@ -129,7 +111,8 @@ class One_Role(Resource):
     
 api.add_resource(One_Role, '/roles/<int:id>')
 
-# ~~~~~~~~~~~~~~~YOU DO~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 class Actors(Resource):
     def get(self):
         q = Actor.query.all()
@@ -140,7 +123,6 @@ class Actors(Resource):
     
     def post(self):
         data = request.get_json()
-        # 10b. raise unprocessable entity
         try:
             actor = Actor(name=data.get('name'), image=data.get('image'), age=data.get('age'), country=data.get('country'))
             db.session.add(actor)
@@ -167,7 +149,6 @@ class One_Actor(Resource):
     def patch(self, id):
         q = Actor.query.filter_by(id=id).first()
         data = request.get_json()
-        # 10b. raise Unprocessable Entity
         try:
             for attr in data:
                 setattr(q, attr, data.get(attr))
@@ -177,10 +158,9 @@ class One_Actor(Resource):
             raise UnprocessableEntity('no')
         return make_response(q.to_dict(), 200)
 api.add_resource(One_Actor, '/actors/<int:id>')
-#~~~~~~~~~~~~~~~~~~~~~~~~~~END YOU DO~~~~~~~~~~~~~~~~~~~~~~~
 
-# 🛑 you still ned raise() and abort() so view method can exit gracefully
-#7d. create fallback
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 @app.errorhandler(NotFound)
 def handle_not_found(e):
     response = make_response(
--- 04_client_server/server/models.py
+++ 04_client_server/server/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.associationproxy import association_proxy
-# 9a. import validates from sqlalchemy.orm
 from sqlalchemy.orm import validates 
 db = SQLAlchemy()
 
@@ -12,17 +11,14 @@ class Production(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # 8a. add constraints 
-    # 🛑 If you see 'Constraint: must have a name'
-    # 🛑 Add render_as_batch=True in Migrate(app, db, ...) in app.py
-    title = db.Column(db.String, nullable=False, unique=True) # 8a. not null, unique
+    title = db.Column(db.String, nullable=False, unique=True)
     genre = db.Column(db.String) 
     length = db.Column(db.Integer) 
     year = db.Column(db.Integer) 
-    image = db.Column(db.String, nullable=False) # 8a. required
+    image = db.Column(db.String, nullable=False) 
     language = db.Column(db.String)
     director = db.Column(db.String)
-    description = db.Column(db.String(50)) # 8a. at least 50 chars
+    description = db.Column(db.String(50))
     composer = db.Column(db.String)
 
     roles = db.relationship('Role', back_populates='production')
@@ -30,7 +26,7 @@ class Production(db.Model, SerializerMixin):
     
     serialize_rules = ('-created_at', '-updated_at', '-roles.production', '-actors.productions')
 
-    # 9b. validation: image must be 'png', 'jpg', or 'jpeg'
+
     @validates('image')
     def validate_image(self, key, image):
         if image == '':
@@ -39,7 +35,7 @@ class Production(db.Model, SerializerMixin):
             raise ValueError('image must be png or jpg')
         else:
             return image 
-    # 9b. validation: year must be > 1850
+
     @validates('year')
     def validates_year(self, key, year):
         if(year > 1850):
@@ -56,9 +52,9 @@ class Actor(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    #8b. add constraints
-    name = db.Column(db.String, nullable=False, unique=True) # 8b. required, unique
-    image = db.Column(db.String, nullable=False) # 8b. required
+
+    name = db.Column(db.String, nullable=False, unique=True) 
+    image = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
     country = db.Column(db.String)
 
@@ -67,14 +63,12 @@ class Actor(db.Model, SerializerMixin):
 
     serialize_rules = ('-created_at', '-updated_at', '-roles.actor', '-productions.actors')
 
-    # 9c. validation: age must be between 0 and 200
     @validates('age')
     def validate_age(self, key, age):
         if(age < 0 or age > 200):
             raise ValueError('age must be greater than 0 and less than 200')
         else:
             return age
-    # 9d. test in sqlite3 and Postman
         
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -85,14 +79,12 @@ class Role(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
    
-    # 8c. add constraints
-    role_name = db.Column(db.String, nullable=False) # 8c. required
-    # 8d. test in sqlite3 and Postman
+    role_name = db.Column(db.String, nullable=False) 
     
-    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'), nullable=False) # 8c. required
+    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'), nullable=False) 
     production = db.relationship('Production', back_populates='roles')
 
-    actor_id = db.Column(db.Integer, db.ForeignKey('actors.id'), nullable=False) # 8c. required
+    actor_id = db.Column(db.Integer, db.ForeignKey('actors.id'), nullable=False) 
     actor = db.relationship('Actor', back_populates='roles')
 
     serialize_rules = ('-created_at', '-updated_at', '-production.roles', '-actors.roles')
