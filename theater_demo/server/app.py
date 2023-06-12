08d8fbadc95ba37dee2d824f0dcb51b936b86f14
Billy Lowry <billyn.lowry@gmail.com>
2023-06-12 15:15:50 -0400

    Lecture Code

--- theater_demo/server/app.py
+++ theater_demo/server/app.py
@@ -3,11 +3,21 @@
 # 1. Navigate to `models.py`
 
 # 2a. Set Up Imports
-# 2b. Create instance of Flask
-# 2c. Configure the flask app to connect to a database
-# 2d. Enable Alembic by using `Migrate`
-# 2e. Connect the db to the app
+from flask import Flask, request, jsonify, make_response 
+from flask_migrate import Migrate 
+from models import db, Production
 
+# 2b. Create instance of Flask
+app = Flask(__name__)
+# 2c. Configure the flask app to connect to a database
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///app.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
+
+# 2d. Enable Alembic by using `Migrate`
+migrate = Migrate(app, db)
+
+# 2e. Connect the database to the app
+db.init_app(app)
 
 # 3. Migrate the Production model
 
@@ -19,35 +29,73 @@
 # flask db upgrade
 
 
-# 4-5. Navigate to `seed.rb`
+# 4. Navigate to `seed.rb`
 
 # 6. Create a / route that returns Hello World
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
 # 6a. Run `flask run --debug` to check if its in the browser
 
-#7. Create a path to retrieve the longest movie
+#7. Create a path to retrieve the first 5 longest movies
 # 7a. Import jsonify, make_response
 # 7b. Use the `route` decorator
-    # 7c. Query for the longest movie
+@app.route('/longest-movies')
+def get_longest_movies():
+    # 7c. Query for longest movie
+    q = Production.query.order_by(Production.length.desc()).limit(1)
     # 7d. Jsonify and return the response
+    prod = {
+        "title": q[0].title,
+        "genre": q[0].genre,
+        "length": q[0].length
+    }
+    return make_response(jsonify(prod), 200)
 
 # 8. Create a dynamic route
 # 8a. Use the route decorator
+@app.route('/productions/<string:title>')
 # 8b. Create productions() to filter through db
-    # 8c. Return result as JSON
+# 🛑 First run in browser and view the title as response just to show that its working
+def production(title):
+    q = Production.query.filter_by(title=title).first()
+    production_response = {
+        "title": q.title,
+        "genere": q.genre,
+        "length": q.length
+    }
 
+    # 📚 Review With Students: status codes
+    # 🛑 `make_response` will allow us to make a response object with the response body and status code
+    # 🛑 `jsonify` will convert our query into JSON
+
+    # 8c. Return result as JSON
+    return make_response(
+        jsonify(production_response),
+        200
+    )
 
 # 9. View the path and host with request context
 # 9a. Import 'request'
+# 🛑 this is a LocalProxy object from werkzeug
 # 9b. Create route `context` 
+@app.route('/context')
+def context():
     # 9c. use ipdb
     # import ipdb; 
     # ipdb.set_trace()
+    return f'<h1>Path{request.path} Host:{request.host}</h1>'
 
 
 # 10. Use the before_request request hook, what this hook does is up to you. You could hit a breakpoint, print something to server console or anything else you can think of.
+@app.before_request
+def runs_before():
+    current_user={"user_id":1, "username":"rose"}
+    print(current_user)
+
 
 # Note: If you'd like to run the application as a script instead of using `flask run`, uncomment the line below 
 # and run `python app.py`
 
-# if __name__ == '__main__':
-#     app.run(port=5555, debug=True)
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- theater_demo/server/models.py
+++ theater_demo/server/models.py
@@ -2,7 +2,22 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-# 1. Create a Production Model
+# 1. ✅ Create a Production Model
+class Production(db.Model):
+    __tablename__ = "productions"
 
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# 2. navigate to app.py
+    title = db.Column(db.String) 
+    genre = db.Column(db.String)
+    length = db.Column(db.Integer) 
+    year = db.Column(db.Integer) 
+    image = db.Column(db.String)
+    language = db.Column(db.String)
+    director = db.Column(db.String)
+    description = db.Column(db.String) 
+    composer = db.Column(db.String)
+
+# 2. ✅ navigate to app.py
